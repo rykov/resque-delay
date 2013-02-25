fd23d2bc99d2b6bde95716563b45d9f25dc3856f
Kale McNaney <kdmny30@gmail.com>
2013-02-25 07:02:07 -0800

    mongoid 3 fixes

--- lib/resque_delay/performable_method.rb
+++ lib/resque_delay/performable_method.rb
@@ -2,7 +2,8 @@ module ResqueDelay
   class PerformableMethod < Struct.new(:object, :method, :args)
     CLASS_STRING_FORMAT = /^CLASS\:([A-Z][\w\:]+)$/
     AR_STRING_FORMAT    = /^AR\:([A-Z][\w\:]+)\:(\d+)$/
-
+    MG_STRING_FORMAT    = /^MG\:([A-Z][\w\:]+)\:(\w+)$/
+    
     def self.create(object, method, args)
       raise NoMethodError, "undefined method `#{method}' for #{object.inspect}" unless object.respond_to?(method)
       self.new(object, method, args)
@@ -13,28 +14,34 @@ module ResqueDelay
       self.args   = args.map { |a| dump(a) }
       self.method = method.to_sym
     end
+    
+    def resque_args
+      [object, method, args]
+    end
 
     def display_name
       case self.object
       when CLASS_STRING_FORMAT then "#{$1}.#{method}"
       when AR_STRING_FORMAT    then "#{$1}##{method}"
+      when MG_STRING_FORMAT    then "#{$1}##{method}"        
       else "Unknown##{method}"
       end
     end
 
     def perform
       load(object).send(method, *args.map{|a| load(a)})
-    rescue ActiveRecord::RecordNotFound
+    rescue ActiveRecord::RecordNotFound, Mongoid::Errors::DocumentNotFound
       # We cannot do anything about objects which were deleted in the meantime
       true
     end
 
     private
 
-    def load(arg)
+    def load(arg)      
       case arg
       when CLASS_STRING_FORMAT then $1.constantize
       when AR_STRING_FORMAT    then $1.constantize.find($2)
+      when MG_STRING_FORMAT    then $1.constantize.find($2)        
       else arg
       end
     end
@@ -43,8 +50,13 @@ module ResqueDelay
       case arg
       when Class              then class_to_string(arg)
       when ActiveRecord::Base then ar_to_string(arg)
+      when Mongoid::Document  then mg_to_string(arg)
       else arg
       end
+    end
+    
+    def mg_to_string(obj)
+      "MG:#{obj.class}:#{obj.id}"
     end
 
     def ar_to_string(obj)
