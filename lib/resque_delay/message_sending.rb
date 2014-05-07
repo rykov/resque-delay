7206da82654f87ed57e431d089caab877a1b5090
Ben Johnson <bjohnson@binarylogic.com>
2014-05-06 23:32:29 -0400

    remove unused code

--- lib/resque_delay/message_sending.rb
+++ lib/resque_delay/message_sending.rb
@@ -20,30 +20,8 @@ module ResqueDelay
   end
 
   module MessageSending
-    def delay(options = {})
+    def delay_with_resque(options = {})
       DelayProxy.new(self, options)
-    end
-    alias __delay__ delay
-
-    #def send_later(method, *args)
-    #  warn "[DEPRECATION] `object.send_later(:method)` is deprecated. Use `object.delay.method"
-    #  __delay__.__send__(method, *args)
-    #end
-    #
-    #def send_at(time, method, *args)
-    #  warn "[DEPRECATION] `object.send_at(time, :method)` is deprecated. Use `object.delay(:run_at => time).method"
-    #  __delay__(:run_at => time).__send__(method, *args)
-    #end
-
-    module ClassMethods
-      def handle_asynchronously(method)
-        aliased_method, punctuation = method.to_s.sub(/([?!=])$/, ''), $1
-        with_method, without_method = "#{aliased_method}_with_delay#{punctuation}", "#{aliased_method}_without_delay#{punctuation}"
-        define_method(with_method) do |*args|
-          delay.__send__(without_method, *args)
-        end
-        alias_method_chain method, :delay
-      end
     end
   end
 end
